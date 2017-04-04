01335625482900c930ea9ff58acaafab577df250
Tom Valk <tomvalk@gmail.com>
2017-04-04 14:58:11 +0200

    [TASK] Add init of firebase to project.

---
+++ src/app/app.constants.default.ts
@@ -0,0 +1,8 @@
+
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: 'actor-template-7f11b.firebaseapp.com',
+  databaseURL: 'https://actor-template-7f11b.firebaseio.com',
+  storageBucket: 'actor-template-7f11b.appspot.com',
+  messagingSenderId: '',
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+
+import { firebaseConfig } from './app.constants';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -12,7 +15,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
